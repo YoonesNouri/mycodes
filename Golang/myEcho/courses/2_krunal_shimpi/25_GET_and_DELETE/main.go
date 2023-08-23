69d058701b2944a05dbfaf77df0392edbf39ad8d
YoonesNouri <yoones.nouri@gmail.com>
2023-08-23 22:59:49 +0330

    echo

---
+++ Golang/myEcho/courses/2_krunal_shimpi/25_GET_and_DELETE/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"mymodule/2_krunal_shimpi/24_PUT_PATCH_method/config"
+	"mymodule/2_krunal_shimpi/24_PUT_PATCH_method/handlers"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+	"github.com/labstack/gommon/random"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	//* CorrelationID is a request id unique to the request being made
+	CorrelationID = "X-Correlation-ID"
+)
+
+var (
+	c   *mongo.Client
+	db  *mongo.Database
+	col *mongo.Collection
+	cfg config.Properties
+)
+
+func init() {
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		log.Fatalf("Configuration can not be read: %v", err)
+	}
+
+	connectURI := fmt.Sprintf("mongodb://%s:%s", cfg.DBHost, cfg.DBPort)
+	c, err := mongo.Connect(context.Background(), options.Client().ApplyURI(connectURI))
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+	db = c.Database(cfg.DBName)
+	col = db.Collection(cfg.CollectionName)
+}
+
+func addCorrelationID(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		//* generate correlation ID
+		id := c.Request().Header.Get("CorrelationID")
+		var NewID string
+		if id == "" {
+			//* generate a random number
+			NewID = random.String(12)
+
+		} else {
+			NewID = id
+		}
+
+		c.Request().Header.Set("CorrelationID", NewID)
+		c.Response().Header().Set("CorrelationID", NewID)
+		return next(c)
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Logger.SetLevel(log.ERROR)
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Pre(addCorrelationID)
+	h := &handlers.ProductHandler{Col: col}
+	e.PUT("/products", h.UpdateProduct, middleware.BodyLimit("1M"))
+	e.POST("/products", h.CreateProducts, middleware.BodyLimit("1M"))
+	e.GET("/products", h.GetProducts)
+	e.Logger.Infof("Listening on %s:%s", cfg.Host, cfg.Port)
+	e.Logger.Fatal(e.Start(fmt.Sprintf("%s:%s", cfg.Host, cfg.Port)))
+}
