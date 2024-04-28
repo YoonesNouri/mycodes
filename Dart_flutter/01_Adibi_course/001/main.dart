6e377925d11aeef869cd407086eb638774fd4bdd
YoonesNouri <yoones.nouri@gmail.com>
2024-04-28 15:34:04 +0330

    flutter_course

---
+++ Dart_flutter/01_Adibi_course/001/main.dart
@@ -0,0 +1,13 @@
+void main() {
+  int age = 35;
+  String name = "yoones";
+  double grade = 19.99;
+  print("name: $name \ngrade: $grade \nage: $age\n\n");
+
+  // or
+
+  var age2 = 36;
+  var name2 = "nouri";
+  var grade2 = 19.999;
+  print("name2: $name2 \ngrade2: $grade2 \nage2: $age2");
+}
--- Golang/0-exampleTest/main.go
+++ Golang/0-exampleTest/main.go
@@ -1,29 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
-// generateOTP generates a random OTP of the specified length
-func generateOTP(length int) string {
-	rand.Seed(time.Now().UnixNano())
-
-	// Define the character set for the OTP
-	charSet := "0123456789qwertyuioplkjhgfdsazxcvbnm" // You can extend this to include letters and symbols if needed
-
-	otp := make([]byte, length)
-	for i := range otp {
-		otp[i] = charSet[rand.Intn(len(charSet))]
-	}
-	return string(otp)
-}
+import "fmt"
 
 func main() {
-	// Generate a OTP
-	otp := generateOTP(8)
-
-	// Print the OTP
-	fmt.Println("One-Time Password (OTP):", otp)
+    // Creating a map with an initial capacity of 10
+    m := make(map[string]int, 10)
+    m["one"] = 1
+    m["two"] = 2
+    fmt.Println("Map:", m)
 }
